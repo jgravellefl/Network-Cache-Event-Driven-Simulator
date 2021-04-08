f18a96ed73e9863b611c2f4eb30ba306492fc6d3
Joe Gravelle <jgravelle@ufl.edu>
2021-04-08 12:39:39 -0400

    we don't need fileMap anymore
    getting rid of fileMap

--- FileMap/FileMap.cpp
+++
@@ -1,8 +0,0 @@
-#include "FileMap.h"
-
-FileMap::FileMap(int numFiles, float* fileSizes, float* filePopularities){
- this->numFiles = numFiles;
- this->fileSizes = fileSizes;
- this->filePopularities = filePopularities;
-}
-FileMap::FileMap(){};
